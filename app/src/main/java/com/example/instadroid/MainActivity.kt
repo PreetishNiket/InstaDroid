9f21eac6c39b18012011b74f717712cd06b7fb68
Preetish Niket <niketpreetish@gmail.com>
2020-10-21 16:23:33 +0530

    Crated different Fragments

--- app/src/main/java/com/example/instadroid/MainActivity.kt
+++ app/src/main/java/com/example/instadroid/MainActivity.kt
@@ -8,6 +8,10 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.instadroid.ui.HomeFragment
+import com.example.instadroid.ui.NotificationsFragment
+import com.example.instadroid.ui.ProfileFragment
+import com.example.instadroid.ui.SearchFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,29 +19,24 @@ class MainActivity : AppCompatActivity() {
     private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.nav_home -> {
-               // moveToFragment(HomeFragment())
-                message.text="Home"
+                moveToFragment(HomeFragment())
                 return@OnNavigationItemSelectedListener true
             }
             R.id.nav_search -> {
-                //moveToFragment(SearchFragment())
-                message.text="Search"
+                moveToFragment(SearchFragment())
                 return@OnNavigationItemSelectedListener true
             }
             R.id.nav_add_post -> {
-                message.text="Add Post"
                 //item.isChecked = false
                // startActivity(Intent(this@MainActivity, AddPostActivity::class.java))
                 return@OnNavigationItemSelectedListener true
             }
             R.id.nav_notifications -> {
-                message.text="Notifications"
-                //moveToFragment(NotificationsFragment())
+                moveToFragment(NotificationsFragment())
                 return@OnNavigationItemSelectedListener true
             }
             R.id.nav_profile -> {
-                message.text="Profile"
-               // moveToFragment(ProfileFragment())
+                moveToFragment(ProfileFragment())
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -50,13 +49,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
         navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
-        //moveToFragment(HomeFragment())
+        moveToFragment(HomeFragment())
     }
 
     private fun moveToFragment(fragment: Fragment)
     {
         val fragmentTrans = supportFragmentManager.beginTransaction()
-       // fragmentTrans.replace(R.id.fragment_container, fragment)
+        fragmentTrans.replace(R.id.fragment_container, fragment)
         fragmentTrans.commit()
     }
 }
