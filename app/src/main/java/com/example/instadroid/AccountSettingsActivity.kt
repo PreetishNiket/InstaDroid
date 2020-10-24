90df6b092280faad224fa87c5009d6f79e9a694c
Preetish Niket <niketpreetish@gmail.com>
2020-10-25 00:35:14 +0530

    Added Code for SignOut

--- app/src/main/java/com/example/instadroid/AccountSettingsActivity.kt
+++ app/src/main/java/com/example/instadroid/AccountSettingsActivity.kt
@@ -1,14 +1,25 @@
 package com.example.instadroid
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.instadroid.auth.SignInActivity
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_account_settings.*
 
 class AccountSettingsActivity : AppCompatActivity() {
+    private val auth by lazy {
+        FirebaseAuth.getInstance()
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_account_settings)
         save_info_profile_btn.setOnClickListener {
+            finish()
+        }
+        logout_btn.setOnClickListener {
+            auth.signOut()
+            startActivity(Intent(this,SignInActivity::class.java))
             finish()
         }
     }
