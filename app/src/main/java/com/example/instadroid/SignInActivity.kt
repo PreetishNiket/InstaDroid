19fb07c6f4075fcfd657142faed5ee04d61c48a2
Preetish Niket <niketpreetish@gmail.com>
2020-10-24 14:09:16 +0530

    Working on Auth activities

---
+++ app/src/main/java/com/example/instadroid/SignInActivity.kt
@@ -0,0 +1,16 @@
+package com.example.instadroid
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_sign_in.*
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+        signUp_btn.setOnClickListener {
+            startActivity(Intent(this, SignUpActivity::class.java))
+        }
+    }
+}
