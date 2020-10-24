1a1abb21012bcf2fb80aa39fc5997ba0fdf9e6f6
Preetish Niket <niketpreetish@gmail.com>
2020-10-25 00:16:28 +0530

    Added Code for SignUp

--- app/src/main/java/com/example/instadroid/auth/SignInActivity.kt
+++ app/src/main/java/com/example/instadroid/auth/SignInActivity.kt
@@ -5,9 +5,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.instadroid.MainActivity
 import com.example.instadroid.R
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_sign_in.*
 
 class SignInActivity : AppCompatActivity() {
+    private val auth by lazy {
+        FirebaseAuth.getInstance()
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
@@ -16,6 +20,14 @@ class SignInActivity : AppCompatActivity() {
             finish()
         }
         login_btn.setOnClickListener {
+//            startActivity(Intent(this, MainActivity::class.java))
+//            finish()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (auth.currentUser!=null){
             startActivity(Intent(this, MainActivity::class.java))
             finish()
         }
