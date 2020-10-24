9a6525d1f5d35b48365266a9fa6f7567069e2908
Preetish Niket <niketpreetish@gmail.com>
2020-10-25 00:48:18 +0530

    Added Code for SignIn

--- app/src/main/java/com/example/instadroid/auth/SignInActivity.kt
+++ app/src/main/java/com/example/instadroid/auth/SignInActivity.kt
@@ -3,15 +3,20 @@ package com.example.instadroid.auth
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
+import android.widget.Toast
 import com.example.instadroid.MainActivity
 import com.example.instadroid.R
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_sign_in.*
+import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignInActivity : AppCompatActivity() {
     private val auth by lazy {
         FirebaseAuth.getInstance()
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
@@ -20,14 +25,38 @@ class SignInActivity : AppCompatActivity() {
             finish()
         }
         login_btn.setOnClickListener {
-//            startActivity(Intent(this, MainActivity::class.java))
-//            finish()
+            loginUser()
+        }
+
+    }
+
+    private fun loginUser() {
+        val email = email_login.text.toString()
+        val password = pass_login.text.toString()
+        when {
+            TextUtils.isEmpty(email) -> email_signUp.error = "This field is required"
+            TextUtils.isEmpty(password) -> pass_signUp.error = "This field is required"
+            else -> {
+                pBar.visibility = View.VISIBLE
+                auth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener {
+                        if (it.isSuccessful) {
+                            startActivity(Intent(this, MainActivity::class.java))
+                            finish()
+                        } else {
+                            val message = it.exception.toString()
+                            Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+                            pgBar.visibility = View.GONE
+                            auth.signOut()
+                        }
+                    }
+            }
         }
     }
 
     override fun onStart() {
         super.onStart()
-        if (auth.currentUser!=null){
+        if (auth.currentUser != null) {
             startActivity(Intent(this, MainActivity::class.java))
             finish()
         }
