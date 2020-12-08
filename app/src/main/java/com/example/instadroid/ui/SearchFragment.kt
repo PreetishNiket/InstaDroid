a0e76227490cb3ac7f75a65fcca3b4e3c816ef00
Preetish Niket <niketpreetish@gmail.com>
2020-12-08 17:01:41 +0530

    Retrieving the users in RV

--- app/src/main/java/com/example/instadroid/ui/SearchFragment.kt
+++ app/src/main/java/com/example/instadroid/ui/SearchFragment.kt
@@ -1,60 +1,54 @@
 package com.example.instadroid.ui
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.instadroid.R
+import com.example.instadroid.adapter.UserAdapter
+import com.example.instadroid.modelClass.User
+import kotlinx.android.synthetic.main.fragment_search.view.*
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SearchFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class SearchFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
+    private var userList: MutableList<User>? = null
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SearchFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SearchFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+        userList = ArrayList()
+        val view = inflater.inflate(R.layout.fragment_search, container, false)
+        view.rv_search.layoutManager = LinearLayoutManager(context)
+        view.rv_search.adapter = context?.let { UserAdapter(it, userList as ArrayList<User>, true) }
+        view.search_edit_text.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                if (view.search_edit_text.text.toString() == "") {
+                    Toast.makeText(view.context, "Empty", Toast.LENGTH_SHORT).show()
+                } else {
+                    view.rv_search.visibility = View.VISIBLE
+                    retrieveUsers()
                 }
             }
+
+            override fun afterTextChanged(p0: Editable?) {}
+        })
+        return view
+    }
+
+    private fun retrieveUsers() {
+
     }
 }
