4662a73613c3363837927e880dfb46f970449044
Preetish Niket <niketpreetish@gmail.com>
2020-12-12 14:42:01 +0530

    Changed user Class

--- app/src/main/java/com/example/instadroid/modelClass/User.kt
+++ app/src/main/java/com/example/instadroid/modelClass/User.kt
@@ -1,7 +1,20 @@
 package com.example.instadroid.modelClass
 
-data class User(val bio:String,
-                val fullName:String,
-                val image: String,
-                val userName:String,
-                val uid:String)
+class User{
+    var bio:String?=null
+    var fullName:String?=null
+    var image: String?=null
+    var userName:String?=null
+    var uid:String?=null
+    constructor(){
+
+    }
+
+    constructor(bio: String?, fullName: String?, image: String?, userName: String?, uid: String?) {
+        this.bio = bio
+        this.fullName = fullName
+        this.image = image
+        this.userName = userName
+        this.uid = uid
+    }
+}
